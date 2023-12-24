18335c0c96f0ce9c0c092b7220f0cb59ed325383
kenil-kanani <kenilkananigecbvn@gmail.com>
2023-12-24 21:58:40 +0530

    Added custom ErrorHandler.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// require('dotenv').config({ path: './.env' })
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env'
@@ -8,7 +8,18 @@ dotenv.config({
 
 
 connectDB()
-
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error : " + error)
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Listening on port ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    })
 
 
 
